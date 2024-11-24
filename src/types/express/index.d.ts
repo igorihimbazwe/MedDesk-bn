9f042652d9b03a2add3667cfdacfa8b1c3b0acbc
Igor IHIMBAZWE <133147435+igorihimbazwe@users.noreply.github.com>
2024-11-24 21:11:25 +0200

    Update index.d.ts

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,16 +1,16 @@
-// import { Request } from "express";
+import { Request } from "express";
 
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       user?: {
-//         id: string;
-//         name: string;
-//         email: string;
-//         role: string;
-//       };
-//     }
-//   }
-// }
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        name: string;
+        email: string;
+        role: string;
+      };
+    }
+  }
+}
 
 
